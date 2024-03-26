92a810b6ef50decaa441db0d1dbb82777876ac5d
Lr2l1 <hoit5it@naver.com>
2024-03-26 16:36:43 +0900

    Modify Damageable and attack

--- zombie/src/zombie/Hero.java
+++ zombie/src/zombie/Hero.java
@@ -2,42 +2,50 @@ package zombie;
 
 import java.util.Random;
 
-public class Hero extends Unit implements Move, Recovery {
+public class Hero extends Human implements Move, Recovery {
 	private Random ran = new Random();
-	private int portion;
+	int portion;
 
-	public Hero(int location, String name, int hp, int power, int portion) {
+	public Hero() {
 		super(1, "용사님", 200, 20);
 		this.portion = 5;
-
 	}
 
 	@Override
 	void attack(Unit zombie) {
-		int ranPower = ran.nextInt(super.getPower());
-		zombie.setHp(zombie.getHp() - ranPower);
-		
+
+		super.setPower(ran.nextInt(super.MAX_POWER));
+		zombie.setHp(zombie.getHp() - super.getPower());
+		System.out.printf("용사의 %d 데미지의 공격!\n", super.getPower());
+
 	}
 
 	@Override
 	void Damageable(Unit zombie) {
-		System.out.printf("%d의 데미지를 받았습니다.", zombie.getPower());
 		super.setHp(super.getHp() - zombie.getPower());
+
+		if (super.getHp() < 0) {
+			super.setHp(0);
+			super.setIsDead();
+		}
+
+		System.out.println(zombie);
 
 	}
 
 	@Override
 	public void move() {
 		super.setLocation();
+		System.out.printf("현재위치 [%d]\n", super.getLocation());
 	}
 
 	@Override
 	public void recover() {
-		if (portion > 0) {
+		if (portion > 0 && super.getHp() <= 100) {
 			super.setHp(super.getHp() + 100);
 			System.out.println("HP가 100 회복되었습니다.");
 		} else
-			System.err.println("포션이 모두 떨어졌습니다.");
+			System.err.println("회복이 불가능합니다!");
 
 	}
 
