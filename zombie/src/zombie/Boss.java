bfdf7a00128625b9387e117720c7aa0ba316a414
Lr2l1 <hoit5it@naver.com>
2024-03-26 18:12:13 +0900

    Modify attack : Recover as much stamina as Monster hit

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -13,13 +13,15 @@ public class Boss extends Monster {
 	void attack(Unit hero) {
 		super.setPower(ran.nextInt(super.MAX_POWER));
 		hero.setHp(hero.getHp() - super.getPower());
-		System.out.printf("보스의 %d 데미지의 공격! ", super.getPower());
+		System.out.printf("보스의 %d 데미지의 공격! , %d만큼 체력회복 되었습니다. ", super.getPower(), super.getPower()/2);
 
 		if (hero.getHp() < 0) {
 			hero.setHp(0);
-			
 			hero.setIsDead();
 		}
+		super.setHp(super.getHp() + super.getPower()/2);
+		
+		
 
 		System.out.println(hero);
 	}
