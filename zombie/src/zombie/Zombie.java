3522880a98f9cc532433159460203b6ef784219c
Lr2l1 <hoit5it@naver.com>
2024-03-27 11:57:23 +0900

    Add Overloading : Zombie class

--- zombie/src/zombie/Zombie.java
+++ zombie/src/zombie/Zombie.java
@@ -9,11 +9,16 @@ public class Zombie extends Monster {
 		super(5, "좀비", 100, 10);
 	}
 
+	public Zombie(int location, String name, int hp, int power) {
+		super(location, name, hp, power);
+	}
+
 	@Override
 	void attack(Unit hero) {
 		super.setPower(ran.nextInt(super.MAX_POWER));
 		hero.setHp(hero.getHp() - super.getPower());
-		System.out.printf("좀비의 %d 데미지의 공격! , %d만큼 체력회복 되었습니다. ", super.getPower(), super.getPower() / 2);
+		System.out.printf("%s의 %d 데미지의 공격! , %d만큼 체력회복 되었습니다. ", super.getName(), super.getPower(),
+				super.getPower() / 2);
 
 		if (hero.getHp() < 0) {
 			hero.setHp(0);
@@ -26,18 +31,19 @@ public class Zombie extends Monster {
 
 	@Override
 	void berserk(Human hero) {
-		
+
 		super.setPower(ran.nextInt(super.MAX_POWER));
-		System.out.printf("[광폭화 모드] 좀비의 %d 데미지의 공격! , %d만큼 체력회복 되었습니다. ", super.getPower(), super.getPower() / 2);
+		System.out.printf("[광폭화 모드] %s의 %d 데미지의 공격! , %d만큼 체력회복 되었습니다. ", super.getName(), super.getPower(),
+				super.getPower() / 2);
 		super.setHp(super.getHp() + super.getPower() / 2);
 
-		
 		int ranDice = ran.nextInt(3);
 		if (ranDice == 2) {
 			System.out.println();
 			System.err.println("광폭화 추가타!");
 			super.setPower(ran.nextInt(super.MAX_POWER));
-			System.out.printf("좀비의 %d 데미지의 공격! , %d만큼 체력회복 되었습니다. ", super.getPower(), super.getPower() / 2);
+			System.out.printf("%s의 %d 데미지의 공격! , %d만큼 체력회복 되었습니다. ", super.getName(), super.getPower(),
+					super.getPower() / 2);
 			super.setHp(super.getHp() + super.getPower() / 2);
 		}
 
